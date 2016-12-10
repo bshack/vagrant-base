11b5f9c6dec6b2d4c9aaa52d47cdb59ece3475d3
William Shackelford <williamshackelford@Williams-MacBook-Pro.local>
2016-12-10 02:05:52 -0500

    use debian/contrib-jessie64

--- provision.sh
+++ provision.sh
@@ -1,7 +1,18 @@
+# if you need the vbox additions for network sharing
+#sed -i 's/ main/ main contrib/g' /etc/apt/sources.list;
+#apt-get -y install virtualbox-guest-dkms;
+
+# grab the latest package versions list
 apt-get update;
+# install the latest version of the OS
 apt-get -y dist-upgrade;
+# install the packages
 apt-get -y install curl build-essential vim git memcached python;
+# install mariadb-server in noninteractive mode to prevent the config prompts
 DEBIAN_FRONTEND=noninteractive apt-get install -y mariadb-server;
+# grab the newest version of nodejs
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -;
+# grab the latest package versions list including nodejs
 apt-get update;
+# install nodejs
 apt-get -y install nodejs;
